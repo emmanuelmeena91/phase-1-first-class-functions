f51cc3eee189802a30fdef3f594a0bd519cab6d8
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-06-29 15:28:00 +0300

    1st class complete

---
+++ index.js
@@ -0,0 +1,21 @@
+function receivesAFunction(callback) {
+  callback();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+    
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    // do something
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    // do something
+  };
+}
